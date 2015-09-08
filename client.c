cffc2f983451a7b20b288352223baa01bd4ea2ad
Ashish Nagar <ashishnagar31@gmail.com>
2015-09-08 17:55:37 +0530

    changes

--- client.c
+++ client.c
@@ -1,14 +1,32 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <unistd.h>
 #include <stdio.h>
+#include <time.h>
+static struct timeval tm1;
 
+inline void start()
+{
+    gettimeofday(&tm1, NULL);
+}
+
+inline void stop()
+{
+    struct timeval tm2;
+    gettimeofday(&tm2, NULL);
+
+    unsigned long long t = 1000 * (tm2.tv_sec - tm1.tv_sec) + (tm2.tv_usec - tm1.tv_usec) / 1000;
+    printf("%llu ms\n", t);
+}
 int main(int argc, char**argv)
 {
    int sockfd,n;
+   int num_packets=0;
+   clock_t start_t, end_t, total_t;
    struct sockaddr_in servaddr,cliaddr;
    char sendline[1000];
    char recvline[1000];
-
+   char packet[]="RANDOMDATA\n";
    if (argc != 2)
    {
       printf("usage:  client <IP address>\n");
@@ -26,10 +44,11 @@ int main(int argc, char**argv)
 
    while (fgets(sendline, 10000,stdin) != NULL)
    {
-      sendto(sockfd,sendline,strlen(sendline),0,
+	  start();
+	  sendto(sockfd,packet,strlen(packet),0,
              (struct sockaddr *)&servaddr,sizeof(servaddr));
       n=recvfrom(sockfd,recvline,10000,0,NULL,NULL);
-      recvline[n]=0;
-      fputs(recvline,stdout);
+      printf("Packets Sent : %d\n",++num_packets);
+      stop();
    }
 }
--- server.c
+++ server.c
@@ -2,45 +2,46 @@
 #include <netinet/in.h>
 #include <stdio.h>
 
-int main(int argc, char**argv)
-{
-   int listenfd,connfd,n;
-   struct sockaddr_in servaddr,cliaddr;
-   socklen_t clilen;
-   pid_t     childpid;
-   char mesg[1000];
+int main(int argc, char**argv) {
+	int listenfd, connfd, n;
+	struct sockaddr_in servaddr, cliaddr;
+	socklen_t clilen;
+	pid_t childpid;
+	int num_packets = 0;
+	char mesg[1000];
 
-   listenfd=socket(AF_INET,SOCK_STREAM,0);
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
 
-   bzero(&servaddr,sizeof(servaddr));
-   servaddr.sin_family = AF_INET;
-   servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
-   servaddr.sin_port=htons(32000);
-   bind(listenfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(32000);
+	bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
 
-   listen(listenfd,1024);
+	listen(listenfd, 1024);
 
-   for(;;)
-   {
-      clilen=sizeof(cliaddr);
-      connfd = accept(listenfd,(struct sockaddr *)&cliaddr,&clilen);
+	for (;;) {
+		clilen = sizeof(cliaddr);
+		connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &clilen);
 
-      if ((childpid = fork()) == 0)
-      {
-         close (listenfd);
+		if ((childpid = fork()) == 0) {
+			close(listenfd);
+			num_packets = 0;
+			for (;;) {
+				n = recvfrom(connfd, mesg, 1000, 0,
+						(struct sockaddr *) &cliaddr, &clilen);
+				if (n != 0) {
+					sendto(connfd, mesg, n, 0, (struct sockaddr *) &cliaddr,
+							sizeof(cliaddr));
+					mesg[n] = 0;
+					printf("Packets received : %d\n", ++num_packets);
+					printf("%s", mesg);
 
-         for(;;)
-         {
-            n = recvfrom(connfd,mesg,1000,0,(struct sockaddr *)&cliaddr,&clilen);
-            sendto(connfd,mesg,n,0,(struct sockaddr *)&cliaddr,sizeof(cliaddr));
-            printf("-------------------------------------------------------\n");
-            mesg[n] = 0;
-            printf("Received the following:\n");
-            printf("%s",mesg);
-            printf("-------------------------------------------------------\n");
-         }
+				}
 
-      }
-      close(connfd);
-   }
+			}
+
+		}
+		close(connfd);
+	}
 }
