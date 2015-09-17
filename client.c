ef5c819ed0c68e779c27f1a191cab42a54679a15
Ashish Nagar <ashishnagar31@gmail.com>
2015-09-17 22:21:48 +0530

    logging

--- client.c
+++ client.c
@@ -1,10 +1,11 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-static struct timeval tm1;
-
+static struct timeval tm1,tm2;
+unsigned long long t ;
 inline void start()
 {
     gettimeofday(&tm1, NULL);
@@ -12,21 +13,29 @@ inline void start()
 
 inline void stop()
 {
-    struct timeval tm2;
-    gettimeofday(&tm2, NULL);
 
-    unsigned long long t = 1000 * (tm2.tv_sec - tm1.tv_sec) + (tm2.tv_usec - tm1.tv_usec) / 1000;
-    printf("%llu ms\n", t);
+    gettimeofday(&tm2, NULL);
+    t = 1000 * (tm2.tv_sec - tm1.tv_sec) + (tm2.tv_usec - tm1.tv_usec) / 1000;
+
+}
+unsigned int randr(unsigned int min, unsigned int max)
+{
+       double scaled = (double)rand()/RAND_MAX;
+
+       return (max - min +1)*scaled + min;
 }
 int main(int argc, char**argv)
 {
    int sockfd,n;
+   FILE *fp;
    int num_packets=0;
    clock_t start_t, end_t, total_t;
    struct sockaddr_in servaddr,cliaddr;
+   time_t ltime;
    char sendline[1000];
    char recvline[1000];
-   char packet[]="RANDOMDATA\n";
+   char filename[]="log.csv";
+   char packet[]="RANDOMDATA\n";  // 11 bytes of payload
    if (argc != 2)
    {
       printf("usage:  client <IP address>\n");
@@ -42,13 +51,18 @@ int main(int argc, char**argv)
 
    connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
 
-   while (fgets(sendline, 10000,stdin) != NULL)
+   while (1)
    {
+	  fp=fopen(filename,"w+");
+	  ltime=time(NULL);
 	  start();
 	  sendto(sockfd,packet,strlen(packet),0,
              (struct sockaddr *)&servaddr,sizeof(servaddr));
       n=recvfrom(sockfd,recvline,10000,0,NULL,NULL);
-      printf("Packets Sent : %d\n",++num_packets);
       stop();
+      printf("%s--Latentcy : %llu ms\n",asctime( localtime(&ltime) ),t);
+      fprintf(fp,"%s--Latentcy : %llu ms\n",asctime( localtime(&ltime) ),t);
+      fclose(fp);
+      usleep(randr(60,300)*1000);
    }
 }
